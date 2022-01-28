017e5ceec8afa9619a920ea9f8165e6df33f041e
J-Revel <j.revel@protonmail.com>
2022-01-28 22:26:33 +0100

    Added InputSystem

---
+++ Project/Assets/Scripts/Character/CharacterTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterTest : MonoBehaviour
+{
+    public InputAction verticalAction;
+    public InputAction horizontalAction;
+    
+    void Start()
+    {
+        verticalAction.Enable();
+        horizontalAction.Enable();
+    }
+
+    void Update()
+    {
+        Debug.Log(verticalAction.ReadValue<float>() + " " + horizontalAction.ReadValue<float>());
+    }
+}
