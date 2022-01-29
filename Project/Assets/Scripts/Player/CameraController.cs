9fe8dda75c1dfb61c085c8bab3bfe135fb332f8b
J-Revel <j.revel@protonmail.com>
2022-01-29 16:08:24 +0100

    Added light cone mesh generator

---
+++ Project/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float deltaAngle = 30;
+    public float startAngle = 0;
+    public int subdivisions = 10;
+    public LayerMask layerMask;
+    public bool updateMesh = false;
+    public MeshFilter meshFilter;
+    public float zPosition = 0;
+    public Transform targetElement;
+    
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        if(updateMesh)
+        {
+            UpdateMesh();
+        }   
+    }
+
+    public void Update()
+    {
+        if(updateMesh)
+        {
+            UpdateMesh();
+        }   
+    }
+
+    private Vector3 ProjectOnGamePlane(Vector3 pos)
+    {
+        Vector3 cameraPosition = Camera.main.transform.position;
+        Vector3 cameraDirection = cameraPosition - pos;
+        float targetDistance = (pos.z - zPosition) * cameraDirection.magnitude / (pos.z - cameraPosition.z); 
+        Vector3 targetPosition = pos + cameraDirection.normalized * targetDistance;
+        return targetPosition;
+    }
+
+    public void UpdateMesh()
+    {
+        Mesh mesh = new Mesh();
+        Vector3 scale = transform.lossyScale;
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+
+        Vector3 startPosition = transform.position;
+        Vector3 targetPosition = ProjectOnGamePlane(startPosition);
+        vertices.Add(targetPosition - transform.position);
+
+        for(int i=0; i<subdivisions; i++)
+        {
+            float angle = ((float)i / subdivisions - 0.5f) * deltaAngle;
+            RaycastHit hit;
+            Vector3 direction = Quaternion.AngleAxis(angle, Vector3.forward) * (ProjectOnGamePlane(targetElement.position) - targetPosition).normalized;
+            Vector3 target = targetPosition + direction * 100;
+            if(Physics.Raycast(targetPosition, direction, out hit, 100, layerMask))
+            {
+                target = hit.point;
+            }
+            vertices.Add(target - transform.position);
+            if(i > 0)
+            {
+                triangles.Add(0);
+                triangles.Add(i);
+                triangles.Add(i - 1);
+            }
+        }
+        
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+        
+        meshFilter.mesh = mesh;
+    }
+
+}
---
+++ Project/Assets/Scripts/Render/LightConeMesh.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightConeMesh : MonoBehaviour
+{
+    MeshFilter meshFilter;
+
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
