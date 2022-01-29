4fd66e351c9e943457ecdc024132f26ae046b5f1
Jacques CHAMPELY <jacques.champely@geode.com>
2022-01-29 13:23:07 +0100

    Physics implementation v2

--- Project/Assets/Scripts/Player/PlayerProperties.cs
+++ Project/Assets/Scripts/Player/PlayerProperties.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerProperties", menuName = "ScriptableObjects/PlayerProperties", order = 1)]
 public class PlayerProperties : ScriptableObject
 {
-    public float speed = 10f;
+    [HeaderAttribute("Speeds")]
+    public float speed = 3000f;
+    public float airSpeed = 1200f;
 
-    public float airSpeed = 2f;
-    public float jumpForce = 100f;
-    public float gravityForce = 100f;
-    public float minJumpingTime = 0.5f;
-    public float maxJumpingTime = 1.5f;
+    [HeaderAttribute("Jump")]
+    public float jumpForce = 5000f;
+    public float minJumpingTime = 0.25f;
+    public float maxJumpingTime = 0.4f;
+
+    [HeaderAttribute("SideJump")]
+    public float sideJumpForce = 4000f;
+    public float minSideJumpingTime = 0.15f;
+    public float maxSideJumpingTime = 0.3f;
+
+    [HeaderAttribute("Gravity")]
+    public float gravityForce = 2400f;
+    public float frictionGravityRatio = 0.8f;
 }
